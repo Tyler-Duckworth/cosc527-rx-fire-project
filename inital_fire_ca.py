8a24fb402a844422d7a960ba6b2576b10571b8a8
Tyler Duckworth <tduckworth@agilairellc.onmicrosoft.com>
2025-04-10 14:07:22 -0400

    Added wind field grid.

--- inital_fire_ca.py
+++ inital_fire_ca.py
@@ -10,7 +10,6 @@ from matplotlib import colors
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from collections import deque
-import opensimplex
 
 
 class RabbitQueue:
@@ -37,6 +36,15 @@ Landscape grid - Stores the fuel type (if any) of each one)
 '''
 global fuel_grid 
 
+
+'''
+Wind Grid
+  - Size: ROWSxCOLSx2
+  - Single cell stores u and v components of array
+'''
+global wind_grid
+
+
 global fuel_data # dict of the different fuels available
 global rabbit_queue # stores new rabbits to be added after a step is complete
 
@@ -46,8 +54,14 @@ global wind
 global pressure_grid
 
 
-rabbit_queue = RabbitQueue()
+WIND_VARIABILITY = 3
+SHOW_ANIM = True
 
+rabbit_queue = RabbitQueue()
+nrows = 300
+ncols = 300
+grid_size = (nrows, ncols)
+generations = 100
 wind = (-3, 2)
 fuel_data = {
     1: {"m1": 0.5, "m2": 0.0, "m3": 0.0, "m4": 0.0, "m5": 0.0,
@@ -59,18 +73,18 @@ fuel_data = {
 }
 
 
-def init_grid(grid_size, _type):
-    return np.zeros(grid_size, dtype=_type)
-
-def init_landscape(grid_size):
-    global fuel_grid
-    fuel_grid = np.ones(grid_size)
-
 def init_fires(fires):
     for i, j in fires:
         grid[i, j] = 1
 
-def calc_hop_distance(fuel_id):
+def init_wind_grid(grid_size, seed=None):
+    rand_adjustment = WIND_VARIABILITY * np.random.random(size=(300, 300, 2)) - (WIND_VARIABILITY / 2)
+    wind_grid = np.full((nrows, ncols, 2), wind) * rand_adjustment
+    return wind_grid
+
+wind_grid = init_wind_grid((nrows, ncols, 2))
+
+def calc_hop_distance(i, j, fuel_id):
     fuel = fuel_data.get(fuel_id)
     if fuel is None:
         return 0, 0
@@ -81,8 +95,10 @@ def calc_hop_distance(fuel_id):
     t = 2 * (2 * z_r / 9.81)**0.5
 
     # Equation 1 
-    dx = (C_w * wind[0] * abs(wind[0]) ) * t + C_h * z_r * (0.5 - np.random.random())
-    dy = (C_w * wind[1] * abs(wind[1]) ) * t + C_h * z_r * (0.5 - np.random.random())
+    u = wind_grid[i, j, 0]
+    v = wind_grid[i, j, 1]
+    dx = (C_w * u * abs(v) ) * t + C_h * z_r * (0.5 - np.random.random())
+    dy = (C_w * v * abs(v) ) * t + C_h * z_r * (0.5 - np.random.random())
 
     return dx, dy
 
@@ -90,7 +106,7 @@ def create_child(i, j):
     fuel_type = fuel_grid[i, j]
     if fuel_type == 0: 
         return
-    dx, dy = calc_hop_distance(fuel_type)
+    dx, dy = calc_hop_distance(i, j, fuel_type)
     new_i = int(i + dx)
     new_j = int(j + dy)
 
@@ -119,40 +135,37 @@ def update_grid():
         grid[i, j] = 1
 
 
-if __name__ == "__main__":
-    nrows = 300
-    ncols = 300
-    grid_size = (nrows, ncols)
-    generations = 100
-    
-    init_landscape(grid_size)
-    grid = init_grid(grid_size, int)
-    for i in range(grid_size[0]):
-        for j in range(grid_size[1]):
-            if fuel_grid[i, j] == 0: 
-                grid[i, j] = -1
-    pressure_grid = init_grid(grid_size, float)
-    init_fires([(y, x) for x in range(75, 225) for y in range(200, 210)])
 
-    cmap = colors.ListedColormap(['black', 'green', 'orange', 'red'])
-    norm = colors.BoundaryNorm([-1, 0, 1, 2, 3], cmap.N)   
-    fig, axs = plt.subplots(1, 2, figsize=(12, 6)) 
-    im1 = axs[0].imshow(grid, cmap=cmap, norm=norm, animated=True)
-    axs[0].set_title('Fire Spread')
-    im2 = axs[1].imshow(pressure_grid, cmap='plasma', animated=True)  # Use 'plasma' colormap
-    axs[1].set_title('Pressure Anomalies')
-    
-    def init(): 
-        """ Dummy function (FuncAnimation runs this once before plotting) """
-        return im1, im2, 
-    
-    def update_anim(frame):
-        update_grid()
-        im1.set_array(grid)
-        im2.set_array(pressure_grid)
-        return im1, im2,
+fuel_grid = np.ones(grid_size)
+grid = np.zeros(grid_size, dtype=int)
 
-    anim = animation.FuncAnimation(fig, update_anim, init_func=init, frames=generations, blit=True) 
-    plt.tight_layout()
+for i in range(grid_size[0]):
+    for j in range(grid_size[1]):
+        if fuel_grid[i, j] == 0: 
+            grid[i, j] = -1
+pressure_grid = np.zeros(grid_size, dtype=float)
+init_fires([(y, x) for x in range(75, 225) for y in range(200, 210)])
+cmap = colors.ListedColormap(['black', 'green', 'orange', 'red'])
+norm = colors.BoundaryNorm([-1, 0, 1, 2, 3], cmap.N)   
+fig, axs = plt.subplots(1, 2, figsize=(12, 6)) 
+im1 = axs[0].imshow(grid, cmap=cmap, norm=norm, animated=True)
+axs[0].set_title('Fire Spread')
+im2 = axs[1].imshow(pressure_grid, cmap='plasma', animated=True)  # Use 'plasma' colormap
+axs[1].set_title('Pressure Anomalies')
+
+def init(): 
+    """ Dummy function (FuncAnimation runs this once before plotting) """
+    return im1, im2, 
+
+def update_anim(frame):
+    update_grid()
+    im1.set_array(grid)
+    im2.set_array(pressure_grid)
+    return im1, im2,
+
+anim = animation.FuncAnimation(fig, update_anim, init_func=init, frames=generations, blit=True) 
+plt.tight_layout()
+if SHOW_ANIM:
+    plt.show()    
+else:   
     anim.save("demo.mp4")
-    
