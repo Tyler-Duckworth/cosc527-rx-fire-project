299ea4c48deb8bc5d704c605402d8d2cefe5941d
Tyler Duckworth <tduckworth@agilairellc.onmicrosoft.com>
2025-04-19 20:01:34 -0400

    Separated environment into its own class, added random building drawing

--- fire_ca.py
+++ fire_ca.py
@@ -12,7 +12,7 @@ import matplotlib.animation as animation
 from collections import deque
 from osgeo import gdal
 from perlin_numpy import generate_perlin_noise_3d
-
+import matplotlib.patches as patches
 gdal.UseExceptions()
 
 class RabbitQueue:
@@ -66,24 +66,31 @@ FUEL_DATA = {
     }
 } 
 
+MAX_BUILDINGS = 10
+BUILDING_POOL_DIST = [0.5, 0.25, 0.2, 0.05]
+BUILDING_POOL = [
+    {"width": 5, "height": 7, "name": "Research Station", "limit": 20},
+    {"width": 2, "height": 2, "name": "Fire Tower", "limit": 5},
+    {"width": 2, "height": 3, "name": "Bathroom", "limit": 10},
+    {"width": 30, "height": 30, "name": "Town", "limit": 1}
+]
 
+class Environment:
+    ''' Represents the simulation environment '''
+    def __init__(self, source_file: str):
+        raw_file = gdal.Open(source_file)
+        self.grid_size = (raw_file.RasterXSize, raw_file.RasterYSize)
+        metadata = raw_file.GetMetadata()
+        self.ambtemp = metadata.get("AMBTEMP", DEFAULT_AMBTEMP)
+        self.relhum = metadata.get("RELHUM", DEFAULT_RELHUM)
+        self.soil_moisture = metadata.get("SMAP", DEFAULT_SMAP)
+        wind_arr = metadata.get("WIND", DEFAULT_WIND).split(",")
+        self.wind = (float(wind_arr[0]), float(wind_arr[1]))
 
-class FireGrid:
-    '''
-    Raster Breakdown
-
-    Bands:
-    - 1 - NDVI (Greenness Index, [-1, 1])
-    - 2 - Elevation (??, [0, 2000))
-    - 3 - Slope (degrees, [0, 90])
-    - 4 - Aspect (degrees, [0, 360])
-    '''
-    def __init__(self, input_file_name: str, generations: int):
-        raw_file = gdal.Open(input_file_name)
-        
         # background layer for plot
         self.bg = raw_file.GetRasterBand(1).ReadAsArray()
-        self.generations = generations
+        self.elevation = raw_file.GetRasterBand(3).ReadAsArray()
+        self.elevation[self.elevation < 0] = 0
         # Read in area metadata encoded into TIFF
         metadata = raw_file.GetMetadata()
         self.ambtemp = metadata.get("AMBTEMP", DEFAULT_AMBTEMP)
@@ -109,11 +116,36 @@ class FireGrid:
         ''' Slope Grid - Stores slope as percentage (TODO: Figure out aspect)'''
         self.slope_grid = raw_file.GetRasterBand(3).ReadAsArray() / 100
         
-        self.create_fuel_grid(self.bg)
-        self.init_fires([(y, x) for x in range(200, 250) for y in range(250, 260)])
+        self.init_fuel_grid(self.bg)
         self.init_wind_grid((raw_file.RasterXSize, raw_file.RasterYSize, 2))
-    
-    def create_fuel_grid(self, fuel_band):
+        self.buildings = []
+
+    def draw_buildings(self):
+        ''' Draw the buildings '''
+        num_buildings = np.random.choice(np.arange(2, MAX_BUILDINGS))
+        for i in range(num_buildings):
+            # draw building from pool
+            choice = np.random.choice(np.arange(len(BUILDING_POOL)), p=BUILDING_POOL_DIST)
+            bldg = BUILDING_POOL[choice]
+            if bldg['limit'] == 1:
+                # if no more of a type can be drawn, remove from pool
+                BUILDING_POOL.pop(choice)
+                p = BUILDING_POOL_DIST.pop(choice)
+                BUILDING_POOL_DIST[0] += p
+            else: 
+                bldg['limit'] -= 1
+            # choose coords - start with no constraints (i.e. a building can go anywhere)
+            x, y = np.random.choice(np.arange(self.grid_size[0]), size=2)
+            width = bldg['width']
+            height = bldg['height']
+            # mark terrain
+            self.grid[x:(x+width),y:(y+height)] = -2
+            # add building to internal array
+            self.buildings.append([bldg['name'], x, y, width, height])
+            
+
+
+    def init_fuel_grid(self, fuel_band):
         '''
         Creates fuel grid to store fuel types for all cells. 
         
@@ -129,16 +161,7 @@ class FireGrid:
         self.fuel_grid = sparse_veg + shrubbery + dense_veg
         self.grid[fuel_band <= 0] = -1
         self.non_fuel_mask = fuel_band <= 0
-
-    def init_fires(self, fires):
-        '''
-        Initialized the various fires given an array of coordinates to light. 
-        '''
-        fire_mask = np.zeros(self.grid_size, dtype=bool)
-        fire_mask[tuple(np.array(fires).T)] = True
-        mask = ~self.non_fuel_mask & fire_mask
-        self.grid[mask] = 1
-
+    
     def init_wind_grid(self, grid_size, seed=None):
         '''
         Creates the wind grid for this simulation
@@ -154,6 +177,33 @@ class FireGrid:
         '''
         return WIND_VARIABILITY * self.noise[:, offset:offset+self.grid_size[1], :] - (WIND_VARIABILITY / 2)
     
+    def update_wind_grid(self, i=0):
+        self.wind_grid = np.full((*self.grid_size, 2), self.wind) + self.get_wind_adjustment(i)
+
+class FireSimulation:
+    '''
+    Raster Breakdown
+
+    Bands:
+    - 1 - NDVI (Greenness Index, [-1, 1])
+    - 2 - Elevation (??, [0, 2000))
+    - 3 - Slope (degrees, [0, 90])
+    - 4 - Aspect (degrees, [0, 360])
+    '''
+    def __init__(self, environment: Environment, generations: int):
+        self.env = environment
+        self.generations = generations
+    
+
+    def init_fires(self, fires):
+        '''
+        Initialized the various fires given an array of coordinates to light. 
+        '''
+        fire_mask = np.zeros(self.grid_size, dtype=bool)
+        fire_mask[tuple(np.array(fires).T)] = True
+        mask = ~self.non_fuel_mask & fire_mask
+        self.grid[mask] = 1
+    
     def calc_hop_distance(self, i, j, fuel_id):
         fuel = FUEL_DATA.get(fuel_id)
         if fuel is None:
@@ -165,27 +215,27 @@ class FireGrid:
         z_r = 2 * (0.1 + np.random.random()) * fuel.get("f_h", 1)
         t = 2 * (2 * z_r / 9.81)**0.5  
 
-        u = self.wind_grid[i, j, 0] / CELL_WIDTH
-        v = self.wind_grid[i, j, 1] / CELL_WIDTH
-        s = self.slope_grid[i, j]
+        u = self.env.wind_grid[i, j, 0] / CELL_WIDTH
+        v = self.env.wind_grid[i, j, 1] / CELL_WIDTH
+        s = self.env.slope_grid[i, j]
         dx = (C_w * u * abs(u) + (10 * C_f * s * abs(s))) * t + C_h * z_r * (0.5 - np.random.random())
         dy = (C_w * v * abs(v)  + (10 * C_f * s * abs(s))) * t + C_h * z_r * (0.5 - np.random.random())
 
         return dx, dy
 
     def update_grid(self, frame_no: int):
-        for i in range(self.grid_size[0]):
-            for j in range(self.grid_size[1]):
+        for i in range(self.env.grid_size[0]):
+            for j in range(self.env.grid_size[1]):
                 self.advance_grid_cell(i, j)
         while len(rabbit_queue._queue) > 0:
             i, j = rabbit_queue.get()
-            self.grid[i, j] = 1
-            self.lifespan_grid[i, j] = 1
-        # update wind grid
-        self.wind_grid = np.full((*self.grid_size, 2), self.wind) + self.get_wind_adjustment(frame_no)
+            self.env.grid[i, j] = 1
+            self.env.lifespan_grid[i, j] = 1
+        self.env.update_wind_grid(frame_no)
+        
     
     def create_child(self, i, j):
-        fuel_type = self.fuel_grid[i, j]
+        fuel_type = self.env.fuel_grid[i, j]
         if fuel_type == 0: 
             return
         
@@ -194,53 +244,60 @@ class FireGrid:
         new_j = int(j + dy)
 
         # add to queue 
-        if 0 <= new_i < self.grid_size[0] and 0 <= new_j < self.grid_size[1]:
-            if self.grid[new_i, new_j] == 0:
+        if 0 <= new_i < self.env.grid_size[0] and 0 <= new_j < self.env.grid_size[1]:
+            if self.env.grid[new_i, new_j] == 0:
                 rabbit_queue.put((new_i, new_j))  
 
     def advance_grid_cell(self, i, j):
-        val = self.grid[i, j]
+        val = self.env.grid[i, j]
         if val == 1: # Burning - Produce children, set to dying
             num_children = np.random.randint(1, 6)
             for k in range(num_children):
                 self.create_child(i, j)
             # roll dice - see if it survives this day
-            if np.random.random() <= FUEL_DATA.get(self.fuel_grid[i, j])['p_lifespan'][self.lifespan_grid[i, j] - 1]:
-                self.lifespan_grid[i, j] += 1
+            if np.random.random() <= FUEL_DATA.get(self.env.fuel_grid[i, j])['p_lifespan'][self.env.lifespan_grid[i, j] - 1]:
+                self.env.lifespan_grid[i, j] += 1
             else:
-                self.grid[i, j] = 2
+                self.env.grid[i, j] = 2
         elif val == 2: # dying - mark cell as dead
             self.grid[i, j] = -1
-    def plot_wind(self, ax):
-        ax.quiver(np.arange(0, self.grid_size[1], 10), np.arange(0, self.grid_size[1], 10), self.wind_grid[::10, ::10, 0], self.wind_grid[::10, ::10, 1])
-        return ax
 
 
 def run_simulation(input_file_name="", generations=100):
-    grid = FireGrid(input_file_name, generations)
-    cmap = colors.ListedColormap(['black', 'green', 'orange', 'red'])
-    norm = colors.BoundaryNorm([-1, 0, 1, 2, 3], cmap.N)   
-    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(18,6)) 
+    env = Environment(input_file_name)
+    env.draw_buildings()
+    sim = FireSimulation(env, generations)
+    cmap = colors.ListedColormap(['blue', 'black', 'green', 'orange', 'red'])
+    norm = colors.BoundaryNorm([-2, -1, 0, 1, 2, 3], cmap.N)   
+    fig, axs = plt.subplots(nrows=1, ncols=4, figsize=(24,6)) 
 
-    im1 = axs[0].imshow(grid.bg, cmap='binary', vmin=-1, vmax=1)
-    im2 = axs[0].imshow(grid.grid, alpha=.5, cmap=cmap, norm=norm, animated=True)
+    im1 = axs[0].imshow(env.bg, cmap='binary', vmin=-1, vmax=1)
+    im2 = axs[0].imshow(env.grid, alpha=.5, cmap=cmap, norm=norm, animated=True)
+    patch_list = []
+    for bldg in env.buildings:
+        patch_list.append(axs[0].add_patch(patches.Rectangle((bldg[2], bldg[1]), bldg[4], bldg[3], linewidth=1, edgecolor='r')))
     axs[0].set_title('Fire Spread')
     
-    axs[1].imshow(grid.fuel_grid, cmap=cmap, norm=norm)
+
+    axs[1].imshow(env.fuel_grid, cmap=cmap, norm=norm)
     axs[1].set_title("Fuel Distribution")
 
-    wind_ticks = np.arange(0, grid.grid_size[1], WIND_PLOT_INTERVAL)
-    im3 = axs[2].quiver(wind_ticks, wind_ticks, grid.wind_grid[::WIND_PLOT_INTERVAL, ::WIND_PLOT_INTERVAL, 0], grid.wind_grid[::WIND_PLOT_INTERVAL, ::WIND_PLOT_INTERVAL, 1])
+    wind_ticks = np.arange(0, env.grid_size[1], WIND_PLOT_INTERVAL)
+    im3 = axs[2].quiver(wind_ticks, wind_ticks, env.wind_grid[::WIND_PLOT_INTERVAL, ::WIND_PLOT_INTERVAL, 0], env.wind_grid[::WIND_PLOT_INTERVAL, ::WIND_PLOT_INTERVAL, 1])
     axs[2].set_title("Wind Grid")
-    def init(): 
+
+    elevation = axs[3].imshow(env.elevation, cmap="terrain", vmin=0, vmax=1)
+    fig.colorbar(elevation, ax=axs[3])
+    def init():
         """ Dummy function (FuncAnimation runs this once before plotting) """
-        return im1, im2, im3
+        return im1, im2, im3, *patch_list
 
     def update_anim(frame_no):
-        grid.update_grid(frame_no)
-        im2.set_array(grid.grid)
-        im3.set_UVC(grid.wind_grid[::WIND_PLOT_INTERVAL, ::WIND_PLOT_INTERVAL, 0], grid.wind_grid[::WIND_PLOT_INTERVAL, ::WIND_PLOT_INTERVAL, 1])
-        return im1, im2, im3
+        sim.update_grid(frame_no)
+        im2.set_array(env.grid)
+        im3.set_UVC(env.wind_grid[::WIND_PLOT_INTERVAL, ::WIND_PLOT_INTERVAL, 0], env.wind_grid[::WIND_PLOT_INTERVAL, ::WIND_PLOT_INTERVAL, 1])
+        
+        return im1, im2, im3, *patch_list
 
     anim = animation.FuncAnimation(fig, update_anim, init_func=init, frames=generations, blit=True, repeat=True) 
     plt.tight_layout()
